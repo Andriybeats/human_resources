be3a8e7100a5fcb365f2abaee09a1cfb5c0803ad
Andriybeats <nabivanec.a@gmail.com>
2019-08-12 16:17:58 +0300

    set up urls

--- departments/models.py
+++ departments/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Department(models.Model):
-  name = models.CharField(max_length=20)
-  abbreviation = models.CharField(max_length=3)
+  name = models.CharField(max_length=20, unique=True)
+  abbreviation = models.CharField(max_length=3, unique=True)
--- vacations/models.py
+++ vacations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from employees.models import Employee
 # Create your models here.
+
+class Vacation(models.Model):
+  date_start = models.DateField()
+  date_end = models.DateField()
+  employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
