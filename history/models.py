31d995034db01e76a489833e31fdb7ac2af267ce
Andriybeats <nabivanec.a@gmail.com>
2019-08-12 15:40:59 +0300

    added model and urls for history

--- history/models.py
+++ history/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from employees.models import Employee
+from positions.models import Position
 
 # Create your models here.
+
+class History(models.Model):
+  data_start = models.DateField()
+  data_end = models.DateField()
+  employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+  position = models.ForeignKey(Position, on_delete=models.CASCADE)
